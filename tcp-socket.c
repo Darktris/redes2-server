acc2d967907dd6cb1d6a772dcd395f0723006a41
Daniel Perdices <daniperdices@hotmail.com>
2016-02-02 13:33:06 -0800

    Funciones de TCP con cabeceras doxygen. Faltan pruebas y makefile

--- tcp-socket.c
+++ tcp-socket.c
@@ -1,3 +1,11 @@
+/* vim: set ts=4 sw=4 et: */
+/**
+  @file tcp-socket.c
+  @brief Libreria de manejo de sockets TCP
+  @author Sergio Fuentes  <sergio.fuentesd@estudiante.uam.es>
+  @author Daniel Perdices <daniel.perdices@estudiante.uam.es>
+  @date 2016/02/01
+  */
 #include <stdio.h>          
 #include <stdlib.h>
 #include <sys/types.h>
@@ -6,6 +14,13 @@
 #include <pthread.h>
 #include "tcp-socket.h"
 
+/**
+  @brief Abre un socket TCP 
+  @param serverPort: Puerto desde el que se desea escuchar
+  @param *socketd: Puntero al descriptor del socket
+  @return TCPOK si todo fue correcto
+          TCPERR_ARGS/SOCKET/BIND/LISTEN en caso de error con estás funciones
+*/
 int openTCPSocket(uint16_t serverPort, int* socketd) { 
     struct sockaddr_in server; 
 
@@ -38,23 +53,85 @@ int openTCPSocket(uint16_t serverPort, int* socketd) {
     return TCPOK;
 }
 
-
-int acceptTCPSocket(int socketd, void* (*handleConnection)(void*), tcpsocket_args* user_struct) {
+/**
+  @brief Acepta una conexion socket y lanza un hilo de atencion a la conexión 
+  @param socketd: Descriptor del socket escuchado
+  @param *handleConnection: rutina de atención a la peticion
+  @param *conn_struct: estructura de datos de la conexion
+  @return TCPOK si todo fue correcto
+          TCPERR_ARGS/ACCEPT/PTHREAD en caso de error con estás funciones
+*/
+int acceptTCPSocket(int socketd, void* (*handleConnection)(void*), tcpsocket_args* conn_struct) {
     int acceptd=0;
 
     /* Control de errores */
     if(socketd==-1 || handleConnection==NULL) {
         return TCPERR_ARGS;
     }
-    
-    acceptd=accept(socketd,(struct sockaddr *)&(user_struct->client),&(user_struct->clientlen));
+
+    /* Acepta la conexión */
+    acceptd=accept(socketd,(struct sockaddr *)&(conn_struct->client),&(conn_struct->clientlen));
     if(acceptd==-1) {
         return TCPERR_ACCEPT;
     }
 
-    if(pthread_create(&(user_struct->user_thread),NULL, handleConnection, (void*)user_struct)!=0) {
+    /* Lanza un hilo orientado a esa conexión */
+    if(pthread_create(&(conn_struct->user_thread),NULL, handleConnection, (void*)conn_struct)!=0) {
         return TCPERR_PTHREAD;
     }
+
+    return TCPOK;
+}
+
+/**
+  @brief Envia a traves del socket TCP los datos
+  @param socketd: Descriptor del socket escuchado
+  @param *data: puntero a los datos que se han de enviar
+  @param len: tamano de los datos a enviar
+  @return TCPOK si todo fue correcto
+          TCPERR_ARGS/SEND en caso de error con estás funciones
+*/
+int sendTCPSocket(int socketd, void* data, size_t len) {
+    int flag;
+    int sent;	
+ 
+    /* Control de errores */
+    if(socketd==-1 || data==NULL || !len) {
+        return TCPERR_ARGS;
+    }
+   /* Envia los datos por el socket hasta que se acaban los datos */
+    do {
+        sent=send(socketd,data,len,0); 
+        if(sent==-1) {
+            return TCPERR_SEND;
+        }
+        flag=((len-sent)!=0);
+    } while(flag);
+    return TCPOK;
+}
+
+/**
+  @brief Recibe a traves del socket TCP
+  @param socketd: Descriptor del socket escuchado
+  @param *data: puntero a donde se almacenan los datos
+  @param maxlen: tamaño de la zona de memoria
+  @param *len: puntero a la variable de longitud recibida
+  @return TCPOK si todo fue correcto
+          TCPERR_ARGS/RECV en caso de error con estás funciones
+*/
+int rcvTCPSocket(int socketd, void* data, size_t maxlen, size_t* len) {
+    size_t n;
     
+    /* Control de errores */
+    if(socketd==-1 || data==NULL || !maxlen || len==NULL) {
+        return TCPERR_ARGS;
+    }
+    
+    /* Recepción de los datos */
+    n=recv(socketd,data,maxlen,0);
+    if(n==-1) {
+        return TCPERR_RECV;
+    }
+    *len=n;
     return TCPOK;
 }
--- tcp-socket.h
+++ tcp-socket.h
@@ -18,6 +18,8 @@ typedef struct {
 #define TCPERR_LISTEN -4
 #define TCPERR_PTHREAD -5
 #define TCPERR_ACCEPT -6
+#define TCPERR_SEND -7
+#define TCPERR_RECV -8
 
 /* Constantes de relevancia */
 #define BACKLOG 5
@@ -25,4 +27,6 @@ typedef struct {
 /* Funciones */
 int openTCPSocket(uint16_t serverPort, int* socketd);
 int acceptTCPSocket(int socketd, void* (handleConnection)(void*), tcpsocket_args* user_struct);
+int sendTCPSocket(int socketd, void* data, size_t len);
+int rcvTCPSocket(int socketd, void* data, size_t maxlen, size_t* len);
 #endif
